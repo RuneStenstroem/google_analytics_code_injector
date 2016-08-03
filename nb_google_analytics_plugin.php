1217275420968fbb9c21b2792b1509c183313eb5
Rune Stenstrøm <social@runestenstroem.dk>
2016-08-03 13:04:19 +0200

    Update nb_google_analytics_plugin.php

--- nb_google_analytics_plugin.php
+++ nb_google_analytics_plugin.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Google Analytics Plugin by NewBits
+Plugin Name: Google Analytics Plugin
 Plugin URI: http://www.runestenstroem.dk/2016/08/01/google-analytics-plugin-wordpress/
 Description: This Plugin adds Google Analytics UA tracking code to your WordPress Website
 Version: 1.0
